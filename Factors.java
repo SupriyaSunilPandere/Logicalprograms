700a27761a196402fbe12d78650563c78c9724fb
SupriyaSunilPandere <sspandere22@gmail.com>
2023-06-10 11:06:21 +0530

    Logical programming

---
+++ Factors.java
@@ -0,0 +1,22 @@
+package com.jsp.logicalProgramming;
+
+public class Factors {
+public static void printFactors(int n) {
+		
+		for(int i=1;i<=n;i++) {
+			if(n%i==0) {
+				System.out.println(i);
+			}
+			
+		}
+	}
+	
+	public static void main(String[]args) {
+		
+		printFactors(8);
+	}
+	
+
+	
+
+}
---
+++ Swap2.java
@@ -0,0 +1,21 @@
+	package com.jsp.logicalProgramming;
+
+	
+
+public class Swap2 {
+	
+
+	public static void main(String args[]) {
+		int a=10;
+		int b=20;
+		
+		 a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println(a+" ");
+		System.out.println(b+" ");
+	}
+	}
+
+
